8dc3c9f2a21e58d403b96012627c8b6c82eb20fb
Jörg Grabitzky <joerg@Joergs-MBP.fritz.box>
2021-04-26 08:44:17 +0200

    first commit

---
+++ HB-UNI-RGB-LED-CANDLE.ino
@@ -0,0 +1,149 @@
+//- -----------------------------------------------------------------------------------------------------------------------
+// AskSin++
+// 2018-08-03 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+//- -----------------------------------------------------------------------------------------------------------------------
+
+// define this to read the device id, serial and device type from bootloader section
+// #define USE_OTA_BOOTLOADER
+
+#define USE_BATTERY
+
+#ifdef USE_BATTERY
+#define USE_WOR
+#define BAT_CRITICAL 15
+#define BATTSENS BattSensor<AsyncMeter<InternalVCC>>
+#define MODEL 0x50
+#else
+#define BATTSENS NoBattery
+#define MODEL 0x51
+#endif
+
+#define EI_NOTEXTERNAL
+#include <EnableInterrupt.h>
+#include <SPI.h>
+#include <AskSinPP.h>
+#include <LowPower.h>
+#include <Register.h>
+
+#define WSNUM_LEDS    5          //Anzahl angeschlossener LEDs
+#define WSLED_PIN     9          //GPIO Pin LED Anschluss 
+#define WSLED_ENPIN   7          //Enable Pin StepUp
+#define WSLED_TYPE    WS2812B    //LED Typ
+#define WSCOLOR_ORDER RGB        //Farbreihenfolge
+
+
+#define SLOW_PROGRAM_TIMER     30     //ms Wartezeit für den Übergang
+#define NORMAL_PROGRAM_TIMER   15     //ms Wartezeit für den Übergang
+#define FAST_PROGRAM_TIMER     0      //ms Wartezeit für den Übergang
+#define FIRE_PROGRAM_COOLING   55
+#define FIRE_PROGRAM_SPARKLING 120
+
+
+#define CONFIG_BUTTON_PIN 8
+#define ONBOARD_LED_PIN   4
+
+#include "RGBCtrl.h"
+
+#define PEERS_PER_CHANNEL 4
+
+using namespace as;
+
+// define all device properties
+const struct DeviceInfo PROGMEM devinfo = {
+  {0xF3, MODEL, 0x00},    // Device ID
+  "JPFLCKLED0",           // Device Serial
+  {0xF3, MODEL},          // Device Model
+  0x10,                   // Firmware Version
+  as::DeviceType::Dimmer, // Device Type
+  {0x01, 0x00}            // Info Bytes
+};
+
+typedef AskSin<StatusLed<ONBOARD_LED_PIN>, BATTSENS, Radio<LibSPI<10>, 2>> HalType;
+
+DEFREGISTER(Reg0, MASTERID_REGS, DREG_LOWBATLIMIT)
+class Ws28xxList0 : public RegList0<Reg0> {
+  public:
+    Ws28xxList0(uint16_t addr) : RegList0<Reg0>(addr) {}
+
+    void defaults () {
+      clear();
+      lowBatLimit(27);
+    }
+};
+
+typedef RGBLEDChannel<HalType, PEERS_PER_CHANNEL, Ws28xxList0> ChannelType;
+
+class RGBLEDType : public RGBLEDDevice<HalType, ChannelType, 3, Ws28xxList0> {
+private:
+    uint8_t wsledpin;
+  public:
+    typedef RGBLEDDevice<HalType, ChannelType, 3, Ws28xxList0> DevType;
+    RGBLEDType (const DeviceInfo& i, uint16_t addr) : DevType(i, addr), wsledpin(0) {}
+    virtual ~RGBLEDType () {}
+
+    void initWSLED(uint8_t p) {
+      wsledpin = p;
+      pinMode(wsledpin, OUTPUT); //LED-Kerze - 5V_Enable
+    }
+
+    void enableWSLED(bool b) {
+      digitalWrite(wsledpin, b);
+    }
+
+    virtual void configChanged () {
+      DevType::configChanged();
+#ifdef USE_BATTERY
+      uint8_t lowbat = getList0().lowBatLimit();
+      DDECLN(lowbat);
+      if ( lowbat > 0 ) {
+        battery().low(lowbat);
+      }
+#endif
+    }
+};
+
+HalType hal;
+RGBLEDType sdev(devinfo, 0x20);
+ConfigButton<RGBLEDType> cfgBtn(sdev);
+
+void setup () {
+  DINIT(57600, ASKSIN_PLUS_PLUS_IDENTIFIER);
+  sdev.init(hal);
+  buttonISR(cfgBtn, CONFIG_BUTTON_PIN);
+     // Set frequency for CC1101
+  hal.radio.initReg(CC1101_FREQ2, 0x21);
+  hal.radio.initReg(CC1101_FREQ1, 0x65);
+  hal.radio.initReg(CC1101_FREQ0, 0xBA);
+#ifdef USE_BATTERY
+  hal.activity.stayAwake(seconds2ticks(5));
+  // measure battery every 4 hours
+  hal.battery.init(seconds2ticks(60UL * 60 * 4), sysclock);
+  hal.battery.critical(BAT_CRITICAL);
+#endif
+  sdev.initWSLED(WSLED_ENPIN);
+  sdev.enableWSLED(true);
+  sdev.initDone();
+}
+
+void loop() {
+  bool worked = hal.runready();
+  bool poll = sdev.pollRadio();
+  bool on = (sdev.getCurrentLevel() > 0);
+
+  if (sdev.channel(1).lowBat() == false && hal.battery.low() == true) {
+    sdev.channel(1).lowBat(true);
+    sdev.channel(1).changed(true);
+  }
+
+  if (hal.battery.critical())
+    hal.activity.sleepForever(hal);
+
+#ifdef USE_BATTERY
+  sdev.enableWSLED(on);
+  if ( worked == false && poll == false && (sdev.getCurrentProgram() == 0 || on == false)) {
+    hal.sleep();
+  }
+#endif
+
+  sdev.handleLED();
+}
---
+++ README.md
@@ -0,0 +1,71 @@
+[![Creative Commons Lizenzvertrag](https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+Dieses Werk ist lizenziert unter einer [Creative Commons Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz](http://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+# RGB-LED-Kerze mit Homematic-Anbindung
+
+<img src="images/RGB-LED-Candle_colors.png">
+
+## Beschreibung:
+Diese Projekt beschreibt den Aufbau eines RGB-LED Teelichts / einer RGB-LED Kerze mit Homematic-Anbindung.<br>
+Die Leuchtfarbe ist über die WebUI einstellbar.<br>
+Zusätzlich gibt es ein paar auswählbare Automatikprogramme (Farbdurchlauf / verschiedene Flacker-Programme).<br>
+<br>
+**Folgende Varianten können aufgebaut werden:**<br>
+<br>
+:arrow_right: LED-Teelicht mit bedrahteter 3mm Flacker-LED<br>
+:arrow_right: RGB-LED-Teelicht mit WS2812 LED und Spannungsversorung über Micro-USB<br>
+:arrow_right: RGB-Kerze mit WS2812 LED und Spannungsversorung wahlweise über Batterie ***oder*** Micro-USB
+<br>
+<br>
+Die Spannungsversorung erfolgt bei der Teelicht-Variante über Micro-USB.<br>
+Eine Batterieversion ist aktuell in Evaluierung.<br>
+Da die CR2477 Batterie sich allerdings als ungeeignet erwiesen hat, wird diese eine größere Bauform haben und kein Teelicht mehr sein.<br>
+Zusätzlich besteht auch noch die Möglichkeit die Platinen als "normales" LED-Teelicht mit bedrahteter Flacker-LED zu bestücken und zu betreiben.<br>
+<br>
+
+## Funktionen:
+#### Hardware:
+- Micro-USB-Anschluss zur Spannungsversorgung (je nach Variante).<br>
+- 3pol Schiebeschalter auf der Batterieplatine: Ein- und Ausschalter (trennt die Batterie- bzw. USB-Spannung)<br>
+- Config-Taster (Power-PCB).<br>
+- Reset-Taster (Hauptplatine - optional)<br>
+<br>
+
+#### Software:
+Der Sketch und das Addon für die Homematic wurde von [Jerome](https://github.com/jp112sdl) entwickelt.<br> 
+Die Leuchtfarbe ist über die WebUI einstellbar.<br>
+Zusätzlich gibt es ein paar auswählbare Automatikprogramme (Farbdurchlauf / verschiedene Flacker-Programme)<br>
+<br>
+<img src="images/WebUI_LED-Candle.png" width="900">
+<br>
+
+## Schaltplan:
+[Schematics_LED_RGB_Candle_v2.0.pdf](Schematics_LED_RGB_Candle_v2.0.pdf)<br>
+<br>
+<br>
+
+## Platinen:
+#### Hauptplatine:
+<img src="images/RGB-LED-Candle_Main-PCB.jpg" width="400">
+<br>
+
+#### Power-PCB Teelicht-Version:
+<img src="images/RGB-LED-Candle_USB-Power-PCB.jpg" width="400">
+<br>
+
+Spannungsversorung wahlweise über Batterie (Version mit 3 mm Flacker-LED) ***oder*** Micro-USB (Version mit WS2812 LED).<br>
+
+_Auf dieser Platine wäre ein Step-Up für den Betrieb mit einer CR2477 Batterie vorgesehen._
+<br>
+_Leider hat sich bei den Tests ergeben, dass die Batterie nur wenige Stunden hält. Aus diesem Grund wurde diese Lösung verworfen.<br>
+Deshalb wird die Bestückung von diesem Teil der Platine auch nicht näher beschrieben._
+<br>
+<br>
+
+#### Power-PCB Kerzen-Version:
+<img src="images/RGB-LED-Candle_Batterie-PCB.jpg" width="400">
+<br>
+
+Spannungsversorung wahlweise über Batterie ***oder*** Micro-USB<br>
+<br>
